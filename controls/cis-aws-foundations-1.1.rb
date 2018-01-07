aca0265208f9b705cc0ba12fb6706c8f3df72cae
Aaron Lippold <lippold@gmail.com>
2018-01-07 13:40:33 -0500

    removed resources from the 'libarary' directory and updated the 'inspec.yml' to point to my upstream git repo.
    Signed-off-by: Aaron Lippold <lippold@gmail.com>

--- controls/cis-aws-foundations-1.1.rb
+++ controls/cis-aws-foundations-1.1.rb
@@ -1,32 +1,37 @@
 control "cis-aws-foundations-1.1" do
   title "Avoid the use of the 'root' account"
   desc  "The 'root' account has unrestricted access to all resources in the AWS
-account. It is highly recommended that the use of this account be avoided."
+         account. It is highly recommended that the use of this account be
+         avoided."
   impact 0.5
+
   tag "rationale": "The 'root' account is the most privileged AWS account.
-Minimizing the use of this account and adopting the principle of least
-privilege for access management will reduce the risk of accidental changes and
-unintended disclosure of highly privileged credentials."
+      Minimizing the use of this account and adopting the principle of least
+      privilege for access management will reduce the risk of accidental changes and
+      unintended disclosure of highly privileged credentials."
+
   tag "cis_impact": ""
   tag "cis_rid": "1.1"
   tag "cis_level": 1
-  tag "cis_control_number": ""
-  tag "nist": ""
+  tag "cis_control_number": "5.1"
+  tag "nist": ["AC-6 (9)"]
   tag "cce_id": ""
+
   tag "check": "Implement the Ensure a log metric filter and alarm exist for
-usage of 'root' account recommendation in the Monitoring section of this
-benchmark to receive notifications of root account usage. Additionally,
-executing the following commands will provide ad-hoc means for determining the
-last time the root account was used:
+      usage of 'root' account recommendation in the Monitoring section of this
+      benchmark to receive notifications of root account usage. Additionally,
+      executing the following commands will provide ad-hoc means for determining the
+      last time the root account was used:
 
-'aws iam generate-credential-report
+      'aws iam generate-credential-report
 
-'aws iam get-credential-report --query 'Content' --output text | base64 -d |
-cut -d, -f1,5,11,16 | grep -B1 '<root_account>'
+      'aws iam get-credential-report --query 'Content' --output text | base64 -d |
+      cut -d, -f1,5,11,16 | grep -B1 '<root_account>'
 
-'Note: there are a few conditions under which the use of the root account is
-required, such as requesting a penetration test or creating a CloudFront
-private key."
+      'Note: there are a few conditions under which the use of the root account is
+      required, such as requesting a penetration test or creating a CloudFront
+      private key."
+
   tag "fix": "Follow the remediation instructions of the Ensure IAM policies
-are attached only to groups or roles recommendation"
+      are attached only to groups or roles recommendation"
 end
