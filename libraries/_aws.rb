bc24be88c362e3f8598732ea1c32cb96c07560ba
Rony Xavier <rx294@nyu.edu>
2018-02-21 13:57:39 -0500

    Resource Updates
    Signed-off-by: Rony Xavier <rx294@nyu.edu>

--- libraries/_aws.rb
+++ libraries/_aws.rb
@@ -2,6 +2,7 @@
 # loaded only if AWS resources are needed.
 
 # require 'aws-sdk' # TODO: split once ADK v3 is in use
+require 'aws-sdk' # TODO: split once ADK v3 is in use
 require '_aws_backend_factory_mixin'
 require '_aws_resource_mixin'
 require '_aws_connection'
