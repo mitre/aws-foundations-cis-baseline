97a6e014fa70df40aec5f60866e7a197cdfda638
Rony Xavier <rx294@nyu.edu>
2018-03-29 13:14:13 -0400

    Fix aws_sns_subscription to account for PendingConfirmation
    Signed-off-by: Rony Xavier <rx294@nyu.edu>

--- libraries/aws_sns_subscription.rb
+++ libraries/aws_sns_subscription.rb
@@ -46,6 +46,7 @@ class AwsSnsSubscription < Inspec.resource(1)
   end
 
   def fetch_from_aws
+    return if @arn.eql?('PendingConfirmation')
     @aws_response = AwsSnsSubscription::BackendFactory.create.get_subscription_attributes(subscription_arn: @arn).attributes
     @exists = true
     @owner = @aws_response['Owner']
