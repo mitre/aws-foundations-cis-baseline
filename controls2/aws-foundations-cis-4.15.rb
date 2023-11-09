e5e203e1fc9e386c92edbaadef9291b94e42778a
Eugene Aronne <34140975+ejaronne@users.noreply.github.com>
2023-11-09 11:18:05 -0500

    Update aws-foundations-cis-4.15.rb

--- controls2/aws-foundations-cis-4.15.rb
+++ controls2/aws-foundations-cis-4.15.rb
@@ -154,7 +154,7 @@ aws cloudwatch put-metric-alarm --alarm-name `<organizations_changes>`
 ``` "
   impact 0.5
   ref 'https://docs.aws.amazon.com/awscloudtrail/latest/userguide/cloudwatch-alarms-for-cloudtrail.html:https://docs.aws.amazon.com/organizations/latest/userguide/orgs_security_incident-response.html'
-  tag nist: []
+  tag nist: ['AU-6','AU-6(1)','AU-7(1)']
   tag severity: "medium "
   tag cis_controls: [
     {"8" => ["8.11"]}
