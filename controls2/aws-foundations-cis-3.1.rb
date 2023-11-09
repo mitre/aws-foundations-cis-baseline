a8da48991e096c4c8fc4e2994af12959e9bffb19
Eugene Aronne <34140975+ejaronne@users.noreply.github.com>
2023-11-09 10:31:30 -0500

    Update aws-foundations-cis-3.1.rb

--- controls2/aws-foundations-cis-3.1.rb
+++ controls2/aws-foundations-cis-3.1.rb
@@ -105,9 +105,21 @@ https://docs.aws.amazon.com/AmazonS3/latest/dev/object-lifecycle-mgmt.html "
   desc "default_value", "Not Enabled "
   impact 0.5
   ref 'https://docs.aws.amazon.com/awscloudtrail/latest/userguide/cloudtrail-concepts.html#cloudtrail-concepts-management-events:https://docs.aws.amazon.com/awscloudtrail/latest/userguide/logging-management-and-data-events-with-cloudtrail.html?icmpid=docs_cloudtrail_console#logging-management-events:https://docs.aws.amazon.com/awscloudtrail/latest/userguide/cloudtrail-supported-services.html#cloud-trail-supported-services-data-events'
-  tag nist: []
+  tag nist: ['AU-12']
   tag severity: "medium "
   tag cis_controls: [
     {"8" => ["8.5"]}
   ]
+
+  describe aws_cloudtrail_trails do
+    it { should exist }
+  end
+
+  aws_cloudtrail_trails.names.each do |trail|
+    describe aws_cloudtrail_trail(trail) do
+      it { should be_multi_region_trail }
+      it { should be_logging }
+      it { should have_event_selector_mgmt_events_rw_type_all }
+    end
+  end
 end
