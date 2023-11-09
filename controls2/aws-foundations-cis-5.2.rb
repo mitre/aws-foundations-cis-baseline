e163d5004b220573046f72c3badca722584f82da
Eugene Aronne <34140975+ejaronne@users.noreply.github.com>
2023-11-09 12:07:15 -0500

    Update aws-foundations-cis-5.2.rb

--- controls2/aws-foundations-cis-5.2.rb
+++ controls2/aws-foundations-cis-5.2.rb
@@ -48,9 +48,26 @@ server administration ports through another mechanism before removing access by 
 the 0.0.0.0/0 inbound rule. "
   impact 0.5
   ref 'https://docs.aws.amazon.com/AWSEC2/latest/UserGuide/ec2-security-groups.html#deleting-security-group-rule'
-  tag nist: []
+  tag nist: ['CM-7(1)']
   tag severity: "medium "
   tag cis_controls: [
     {"7" => ["9.2"]}
   ]
+
+  exception_security_group_list = input('exception_security_group_list')
+
+  aws_security_groups.group_ids.each do |group_id|
+    if exception_security_group_list.include?(group_id)
+      describe 'Security Group not inspected because it is defined as an exception' do
+        skip "Security Group:: #{group_id} not inspected because it is defined in exception_security_group_list."
+      end
+    end
+
+    next if exception_security_group_list.include?(group_id)
+
+    describe aws_security_group(group_id) do
+      it { should_not allow_in(port: 22, ipv4_range: '0.0.0.0/0') }
+      it { should_not allow_in(port: 3389, ipv4_range: '0.0.0.0/0') }
+    end
+  end
 end
