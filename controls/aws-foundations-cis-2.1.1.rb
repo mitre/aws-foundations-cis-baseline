195ac4a7d3a49dd55ab9b4818d4bfbeae9681e2a
wdower <57142072+wdower@users.noreply.github.com>
2023-11-27 20:25:17 +0000

    clarifying the Not Applicable statement a bit
    Signed-off-by: wdower <57142072+wdower@users.noreply.github.com>

--- controls/aws-foundations-cis-2.1.1.rb
+++ controls/aws-foundations-cis-2.1.1.rb
@@ -161,8 +161,7 @@ aws s3api put-bucket-policy --bucket <bucket_name>
   failing_buckets = []
   # passed_buckets = []
 
-  # TODO: Does the logic condition here hold and could it be cleaner?
-  only_applicable_if('This control is Non Applicable since no S3 buckets were found.') { !s3_buckets.empty? or !(exempt_buckets - s3_buckets).empty? }
+  only_applicable_if('This control is Non Applicable since no unexempt S3 buckets were found.') { !s3_buckets.empty? or !(exempt_buckets - s3_buckets).empty? }
 
   if input('single_bucket').present?
     failing_buckets << input('single_bucket').to_s unless aws_s3_bucket(bucket_name: input('single_bucket')).has_secure_transport_enabled?
