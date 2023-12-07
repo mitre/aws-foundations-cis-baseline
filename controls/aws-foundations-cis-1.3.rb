5347e46ce8210d87a22e0fb4a3833a26cfc5ae54
Aaron Lippold <lippold@gmail.com>
2023-12-07 09:10:56 -0500

    updated controls for govcloud
    Signed-off-by: Aaron Lippold <lippold@gmail.com>

--- controls/aws-foundations-cis-1.3.rb
+++ controls/aws-foundations-cis-1.3.rb
@@ -1,11 +1,11 @@
-control 'aws-foundations-cis-1.3' do
-  title 'Ensure security questions are registered in the AWS account '
+control "aws-foundations-cis-1.3" do
+  title "Ensure security questions are registered in the AWS account "
   desc "
     The AWS support portal allows account owners to establish security questions that can be used
     to authenticate individuals calling AWS customer service for support. It is recommended
     that security questions be established.
     "
-  desc 'rationale',
+  desc "rationale",
        "When creating a new AWS account, a default super user is automatically created. This account
     is referred to as the 'root user' or 'root' account. It is recommended that the use of this
     account be limited and highly controlled. During events in which the 'root' password is no
@@ -13,7 +13,7 @@ control 'aws-foundations-cis-1.3' do
     through authentication using secret questions and associated answers, to recover 'root'
     user login access.
     "
-  desc 'check',
+  desc "check",
        "**From Console:**
 
     1. Login to the AWS account as the 'root' user
@@ -27,7 +27,7 @@ control 'aws-foundations-cis-1.3' do
     questions.
     6. Click `Save questions` . "
 
-  desc 'fix',
+  desc "fix",
        "**From Console:**
 
     1. Login to the AWS Account as the 'root' user
@@ -50,15 +50,19 @@ control 'aws-foundations-cis-1.3' do
     secure physical location."
 
   impact 0.5
-  tag nist: ['IR-6']
-  tag severity: 'medium '
-  tag cis_controls: [{ '8' => ['17.2'] }]
+  tag nist: ["IR-6"]
+  tag severity: "medium "
+  tag cis_controls: [{ "8" => ["17.2"] }]
 
-  describe 'Control has to be tested manually' do
+  # only_if('AWS GovCloud only allows you to Manually view Account information, please review this requirement in the AWS GovCloud Console.') {
+  #   !aws_sts_caller_identity.govcloud?
+  # }
+
+  describe "Requirement must be tested manually" do
     skip "
-      This control must be manually reviewed.
-      Examine the root account's security challenge questions
-      settings in the AWS Managment console as described in this
-      control's check description."
+      This control must be manually reviewed, AWS does not support validation of the 
+      Security Challenge Questions in the AWS CLI or by an API.
+      
+      Examine the root account's security challenge questions in the AWS Managment Console."
   end
 end
